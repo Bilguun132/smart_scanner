d08af622c77457445bd7ca965eb54bd8a3e0b8b3
Bilguun <bilguun132@gmail.com>
2019-10-25 14:48:35 +0800

    added pagination

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -1,6 +1,26 @@
 const CardService = require("../services/card.service");
+var debug = require("debug")("smartscanner:cardController");
+
 module.exports.getAllCards = async (req, res) => {
-  res.send([]);
+  let pageNum = req.query.PageNum;
+  debug("Page Number is", pageNum);
+  if (pageNum == null || pageNum == undefined) {
+    pageNum = 1;
+  }
+  let pageSize = req.query.pageSize;
+  debug("Page Size is ", pageSize);
+  if (pageSize == null || pageSize == undefined) {
+    pageSize = 20;
+  }
+  let skip = (pageNum - 1) * pageSize;
+
+  CardService.getCards(skip, pageSize)
+    .then(cards => {
+      res.send(cards);
+    })
+    .catch(err => {
+      res.status(err.status).send(err);
+    });
 };
 
 module.exports.createCardWithImage = async (req, res) => {
--- services/card.service.js
+++ services/card.service.js
@@ -1,6 +1,26 @@
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const { Card } = require("../db/models/card.model");
+var debug = require("debug")("smartscanner:cardService");
+/**
+ * @param {Number} skip - pagination to skip
+ * @param {NUmber} limit - number of cards to return
+ */
+
+module.exports.getCards = async (skip, limit) => {
+  debug("Skip is %s limit is %s", skip, limit);
+  return new Promise(async (resolve, reject) => {
+    Card.find({})
+      .skip(skip)
+      .limit(limit)
+      .then(cards => {
+        return resolve(cards);
+      })
+      .catch(err => {
+        return reject({ status: 500, message: err });
+      });
+  });
+};
 
 /**
 @param {object} newCard - new card json body
